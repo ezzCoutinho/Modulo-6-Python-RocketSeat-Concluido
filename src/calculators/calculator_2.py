b563c222602b1332eeda91936463b5b9d3db27d8
ezzcoutinho <lammerlopes@hotmail.com>
2025-03-02 19:12:39 -0300

    Refactor Calculator2 to use DriverHandlerInterface for standard derivation processing

--- src/calculators/calculator_2.py
+++ src/calculators/calculator_2.py
@@ -1,8 +1,10 @@
 from flask import request as FlaskRequest
 from typing import Dict, List
-from ..drivers.numpy_handler import NumpyHandler
+from ..drivers.interfaces.driver_handler_interface import DriverHandlerInterface
 
 class Calculator2:
+  def __init__(self, driver_handler: DriverHandlerInterface) -> None:
+    self.__driver_handler = driver_handler
 
   def calculate(self, request: FlaskRequest) -> Dict: # type: ignore
     body = request.json
@@ -19,9 +21,8 @@ class Calculator2:
     return [float(i) for i in input_data]
   
   def __process_data(self, input_data: List[float]) -> float:
-    numpy_handler = NumpyHandler()
     first_process_result = [(num * 11) ** 0.95 for num in input_data]
-    result = numpy_handler.standard_derivation(first_process_result)
+    result = self.standard_derivation(first_process_result)
     return 1/result 
   
   def __format_response(self, calculated_number: float) -> Dict:
@@ -30,3 +31,6 @@ class Calculator2:
         "Calculator": 2,
         "result": round(calculated_number, 2)
     } }
+  
+  def standard_derivation(self, numbers: List[float]) -> float:
+    return self.__driver_handler.standard_derivation(numbers)
