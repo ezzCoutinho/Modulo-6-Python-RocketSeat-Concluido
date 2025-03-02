adc505783b24935533e6c99fb1d5572d5e6149d3
ezzcoutinho <lammerlopes@hotmail.com>
2025-03-02 19:32:28 -0300

    Add integration test for Calculator2 with MockDriverHandler

--- src/calculators/calculator_2_test.py
+++ src/calculators/calculator_2_test.py
@@ -1,18 +1,39 @@
-from typing import Dict
+from typing import Dict, List
 from .calculator_2 import Calculator2
 from ..drivers.numpy_handler import NumpyHandler
+from ..drivers.interfaces.driver_handler_interface import DriverHandlerInterface
 
 class MockRequest:
   def __init__(self, body: Dict) -> None:
     self.json = body
 
-def test_calculate():
+class MockDriverHandler(DriverHandlerInterface):
+  
+  def standard_derivation(self, numbers: List[float]) -> float:
+    return 3
+
+def test_calculate_integration():
   mock_request = MockRequest(body={"numbers": [2.12, 4.62, 1.32]})
+
   driver = NumpyHandler()
   calculator = Calculator2(driver)
-  response = calculator.calculate(mock_request)
-  print()
-  print(response)
 
-  assert isinstance(response, dict)
-  assert response == {"data": {"Calculator": 2, "result": 0.08}}
+  formated_response = calculator.calculate(mock_request)
+  print()
+  print(formated_response)
+
+  assert isinstance(formated_response, dict)
+  assert formated_response == {"data": {"Calculator": 2, "result": 0.08}}
+
+def test_calculate():
+  mock_request = MockRequest(body={"numbers": [2.12, 4.62, 1.32]})
+
+  driver = MockDriverHandler()
+  calculator = Calculator2(driver)
+
+  formated_response = calculator.calculate(mock_request)
+  print()
+  print(formated_response)
+
+  assert isinstance(formated_response, dict)
+  assert formated_response == {"data": {"Calculator": 2, "result": 0.33}}
