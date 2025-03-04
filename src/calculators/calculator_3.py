e1cb492d3ee6c86826dd134d694f7ccacda89e6d
ezzcoutinho <lammerlopes@hotmail.com>
2025-03-04 16:34:34 -0300

    Refactor error handling in Calculator3 to use specific HTTP exceptions

--- src/calculators/calculator_3.py
+++ src/calculators/calculator_3.py
@@ -1,6 +1,9 @@
 from flask import request as FlaskRequest
 from typing import Dict, List
 from src.drivers.interfaces.driver_handler_interface import DriverHandlerInterface
+from src.errors.http_unprocessable_entity import HttpUnprocessableEntity
+from src.errors.http_bad_request import HttpBadRequest
+
 class Calculator3:
   def __init__(self, driver_handler: DriverHandlerInterface) -> None:
     self.__driver_handler = driver_handler
@@ -18,7 +21,7 @@ class Calculator3:
     
   def __validate_body(self, body: Dict) -> List[float]:
     if "numbers" not in body:
-      raise Exception("body mal formatado!")
+      raise HttpUnprocessableEntity("body mal formatado!")
     
     input_data = body["numbers"]
     return [float(i) for i in input_data]
@@ -34,7 +37,7 @@ class Calculator3:
   
   def __verify_results(self, variance: float, multiplication: float) -> None:
     if variance < multiplication:
-      raise Exception("Falha no processo: Variance menor que multiplication")
+      raise HttpBadRequest("Falha no processo: Variance menor que multiplication")
         
   def __format_response(self, variance: float) -> Dict:
     return {
