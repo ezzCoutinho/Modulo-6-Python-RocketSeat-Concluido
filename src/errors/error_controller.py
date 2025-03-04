9b900baa4428a9e33677842f1fbbe6edfa5a7a15
ezzcoutinho <lammerlopes@hotmail.com>
2025-03-04 16:34:23 -0300

    Add error handling function for custom exceptions in error controller

---
+++ src/errors/error_controller.py
@@ -0,0 +1,25 @@
+from typing import Dict
+from src.errors.http_unprocessable_entity import HttpUnprocessableEntity
+from src.errors.http_bad_request import HttpBadRequest
+
+def handle_errors(error: Exception) -> Dict:
+  if isinstance(error, (HttpUnprocessableEntity, HttpBadRequest)):
+    return {
+      'status_code': error.status_code,
+      'body': {
+        'errors': [{
+          'title': error.name,
+          'detail': error.message
+        }]
+      }
+    }
+  
+  return {
+      'status_code': 500,
+      'body': {
+        'errors': [{
+          'title': 'Internal Server Error',
+          'detail': str(error)
+        }]
+      }
+    }
