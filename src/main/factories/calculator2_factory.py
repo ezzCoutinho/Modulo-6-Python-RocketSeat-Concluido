a6dc8d92a1ed61aa507a010e2b284d2778e04956
ezzcoutinho <lammerlopes@hotmail.com>
2025-03-04 13:29:48 -0300

    Simplify calculator2_factory by instantiating NumpyHandler directly in the return statement

--- src/main/factories/calculator2_factory.py
+++ src/main/factories/calculator2_factory.py
@@ -2,5 +2,5 @@ from src.calculators.calculator_2 import Calculator2
 from src.drivers.numpy_handler import NumpyHandler
 
 def calculator2_factory() -> Calculator2:
-  numpy_handler = NumpyHandler()
-  return Calculator2(numpy_handler)
+  calc = Calculator2(NumpyHandler())
+  return calc
